e71d9f6023b937694a11dc387802db2718060621
kokubum <eskokubum@gmail.com>
2021-04-07 22:37:32 -0300

    test: create all the setup of the tests

--- tests/integration/auth.test.ts
+++ tests/integration/auth.test.ts
@@ -1,4 +1,5 @@
 import request from "supertest";
+import { validate as uuidValidate } from "uuid";
 import { clearTablesContent } from "../helper";
 import app from "../../src/app";
 import "../setup";
@@ -8,14 +9,32 @@ describe("Sign Up", () => {
     await clearTablesContent();
   });
 
-  it("Should register an User and return his information", async () => {
-    const response = await request(app).post("/api/v1/auth/signup").send({
-      firstName: "randomFirstName",
-      lastName: "randomLastName",
-      password: "randomPassword",
-      email: "randomUniqueEmail",
-    });
+  it.only("Should sign up an User and return a valid id", async () => {
+    const { status, body } = await request(app)
+      .post("/api/v1/auth/signup")
+      .send({
+        firstName: "randomFirstName",
+        lastName: "randomLastName",
+        password: "randomPassword",
+        email: "randomUnique@email.com",
+      });
 
-    expect(response.status).toBe(200);
+    expect(status).toBe(201);
+    expect(body.status).toBe("success");
+    expect(uuidValidate(body.data.id)).toBeTruthy();
+  });
+
+  it("Should throw an error if the firstName wasn't sent", async () => {
+    const { status, body } = await request(app)
+      .post("/api/v1/auth/signup")
+      .send({
+        lastName: "randomLastName",
+        password: "randomPassword",
+        email: "randomUnique@email.com",
+      });
+
+    expect(status).toBe(400);
+    expect(body.status).toBe("fail");
+    expect(body.data.firstName).toBe("The firstName field is required");
   });
 });
